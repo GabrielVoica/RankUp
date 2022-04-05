62c7afa50c7c053eb2cab9d7cb542a5a5d19099f
Sergi321 <sergimartinn@gmail.com>
2022-04-05 10:02:08 +0200

    register fixed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { RegisterService } from '../register.service';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -11,9 +12,23 @@ export class RegisterComponent implements OnInit {
 
   registerService: RegisterService;
   loading = environment.loading;
+  imageSrc:any = '';
+  status:boolean = false
 
+  onFileChange(event:any) {
+    this.status = false
+    const file = event.target.files[0];
+    this.status = event.target.files.length>0?true:false
+    if(this.status==true){
+       const reader = new FileReader();
+       reader.readAsDataURL(file);
+       reader.onload = () => {
+          this.imageSrc = reader.result;          
+       }
+    }
+  }
 
-  constructor(register: RegisterService, private renderer: Renderer2) {
+  constructor(register: RegisterService, private renderer: Renderer2, private http: HttpClient) {
     this.registerService = register;
    }
 
@@ -28,7 +43,7 @@ export class RegisterComponent implements OnInit {
 
 
   register(){
-    this.registerService.registerUser({username: this.username, email: this.email, center: this.center, password: this.password, passwordConfirm: this.passwordConfirm});
+    this.registerService.registerUser({username: this.username, email: this.email, center: this.center, password: this.password, passwordConfirm: this.passwordConfirm, imageSrc: this.imageSrc});
     environment.loading = true;
   }
 
