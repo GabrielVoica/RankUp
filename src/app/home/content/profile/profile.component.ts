1fa20767f804c459b0b3beffcf21cbe4c8b68e12
Sergi321 <sergimartinn@gmail.com>
2022-04-06 11:58:21 +0200

    profile user info done

--- src/app/home/content/profile/profile.component.ts
+++ src/app/home/content/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { SessionDataService } from 'src/app/session-data.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +9,21 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  
+  constructor(private http: HttpClient, private session: SessionDataService) { }
   apiURL = environment.apiURL;
   imageSrc:any = '';
+
+  username: string;
+  email: string;
+  name: string;
+  lastname: string;
+
   status:boolean = false
-  constructor(private http: HttpClient) { }
+  id = this.session.getId();
+  pic = this.session.getImage();
+  user = this.session.getUsername();
+  mail = this.session.getEmail();
   ngOnInit(): void { }
   onFileChange(event:any) {
     this.status = false
@@ -26,7 +38,7 @@ export class ProfileComponent implements OnInit {
     }
   }
   submit(){
-    this.http.put(this.apiURL+'app/user?image=image&id=791', {'image':this.imageSrc})
+    this.http.put(this.apiURL+`app/user?nick_name=${this.username}&name=${this.name}&lastname=${this.lastname}&email=${this.email}&image=image&id=${this.id}`, {'image':this.imageSrc})
       .subscribe(res => {
         console.log(res);
         alert('Uploaded Successfully.');
